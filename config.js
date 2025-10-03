d2f3a0b300ebbe332a156b44a1cf0e6ea5174e39
Harinee-3010 <kit27.aids22@gmail.com>
2025-10-03 12:07:29 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+        apiKey: "AIzaSyDavXKiN72deOUUisEDhVwGDC9qfrgj77g",
+        authDomain: "careerpathnavigator-f4fa5.firebaseapp.com",
+        projectId: "careerpathnavigator-f4fa5",
+        storageBucket: "careerpathnavigator-f4fa5.firebasestorage.app",
+        messagingSenderId: "782230825565",
+        appId: "1:782230825565:web:55ad555f8c8e3614237bb4",
+        measurementId: "G-8XTY42FSZJ"
+        };
+const geminiApiKey = "AIzaSyCn9ka2TXm54gvXg1TjIBm6p3vpiE2l808";
